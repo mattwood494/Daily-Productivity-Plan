14ee7d380f256f6246b807b50c65b60cc738bac8
Matthew <matthew.wood_28@outlook.com>
2020-10-20 19:46:06 -0700

    Add the first test commit for my C# project making a productivity app

---
+++ Daily Productivity Application/Daily Productivity Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Daily_Productivity_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
