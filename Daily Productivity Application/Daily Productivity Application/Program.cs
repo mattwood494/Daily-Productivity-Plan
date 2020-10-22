a1da5fad46ee6d566c2f4cb53cc603d289d14c15
Matthew Wood <matthew.wood_28@outlook.com>
2020-10-22 09:49:12 -0700

    Primitive Daily Task Compiler Created
    Console application walks user through compiling a list of all daily tasks that need to be completed

--- Daily Productivity Application/Daily Productivity Application/Program.cs
+++ Daily Productivity Application/Daily Productivity Application/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace Daily_Productivity_Application
 {
@@ -6,7 +9,109 @@ namespace Daily_Productivity_Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Daily Planner!");
+            Console.WriteLine();
+            Console.WriteLine();
+            //Thread.Sleep(TimeSpan.FromSeconds(1));
+            Console.WriteLine("Lets start with some brainstorming...");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //Thread.Sleep(TimeSpan.FromSeconds(3));
+            Console.WriteLine("Let's get everything in our brain out of our brain so that we can think clearly");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            //Thread.Sleep(TimeSpan.FromSeconds(2));
+
+
+
+
+
+
+            bool convertedanswer = true;
+            int taskcounter = 0;
+            string input;
+            string answer;
+            string task;
+            Dictionary<int, string> braindump = new Dictionary<int, string>(20);
+
+
+
+
+
+            Console.WriteLine("Would you like to start?");
+            Console.WriteLine("Type [Yes] or [No]");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            input = Console.ReadLine();
+            answer = input.ToLower();
+            if (answer == "yes")
+            {
+                convertedanswer = true;
+            }
+            else
+            {
+                convertedanswer = false;
+                return;
+            }
+
+            Console.WriteLine("What is one thing you must do today?");
+            while (convertedanswer == true)
+            {
+
+
+
+                if (taskcounter == 0)
+                {
+                    input = Console.ReadLine();
+                    taskcounter++;
+                }
+                task = input;
+                
+                
+
+                
+
+                braindump.Add(taskcounter, task);
+                taskcounter++;
+
+                // Read all items    
+                Console.WriteLine("All Braindumped tasks:");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                for (int i = 0; i < braindump.Count; i++)
+                {
+                    Console.WriteLine("Task {0}: {1}", braindump.ElementAt(i).Key, braindump.ElementAt(i).Value);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("What is another thing on your mind that must be done today? Or press enter if you of emptied your brain of everything that must be done today");
+                input = Console.ReadLine();
+                if (String.IsNullOrWhiteSpace(input))
+                {
+                    convertedanswer = false;
+                }
+                else
+                {
+                    convertedanswer = true;
+                }
+            }
+
+            
+
+
+
+
+
         }
     }
 }
